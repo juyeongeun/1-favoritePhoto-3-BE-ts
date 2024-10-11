91bfc1da91ea6a23783b5f0da8b08be011d1eb66
twh <owootak@naver.com>
2024-10-11 10:13:56 +0900

    feat : 유저 관한 등록/삭제/갱신

--- services/userService.js
+++ services/userService.js
@@ -1,5 +1,28 @@
 import userRepository from "../repositorys/userRepository.js";
+import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
+
+const createToken = (user, type) => {
+  const payload = { userId: user.id, email: user.email }; //jwt 토근 정도에 사용자의 id, email 정보를 담는다.
+  const options = { expiresIn: type ? "1w" : "1h" }; //refresh 토큰의 경우 1주일, access 토근은 1시간의 유효성을 둔다
+  return jwt.sign(payload, process.env.JWT_SECRET, options);
+};
+
+const getUser = async ({ email, password }) => {
+  const user = await userRepository.getByEmail(email);
+
+  if (!user) {
+    const error = new Error("Mot found");
+    error.status = 404;
+    error.data = {
+      message: "등록된 사용자가 없습니다.",
+      email,
+    };
+  }
+
+  await verifyPassword(password, user.password);
+  return filterSensitiveUserData(user);
+};
 
 const getUserByEmail = async (email) => {
   const user = await userRepository.getByEmail(email);
@@ -31,6 +54,30 @@ const getUserById = async (userId) => {
   return filterSensitiveUserData(user);
 };
 
+const refreshToken = async (userId, refreshToken) => {
+  const user = await userRepository.getById(userId);
+
+  if (!user) {
+    const error = new Error("Mot found");
+    error.status = 404;
+    error.data = {
+      message: "등록된 사용자가 없습니다.",
+      userId,
+    };
+  }
+
+  if (refreshToken !== user.refreshToken) {
+    const error = new Error("Unprocessable Entity");
+    error.status = 422;
+    error.data = {
+      message: "리프레쉬 토큰이 유효하지 않습니다.",
+      refreshToken,
+    };
+    throw error;
+  }
+  return true;
+};
+
 const create = async (data) => {
   const user = await userRepository.getByEmail(data.email);
 
@@ -51,8 +98,8 @@ const create = async (data) => {
   return filterSensitiveUserData(createUser);
 };
 
-const updateUser = async ({ id, data }) => {
-  const user = await userRepository.update({ id, data });
+const updateUser = async (id, data) => {
+  const user = await userRepository.update(id, data);
 
   return filterSensitiveUserData(user);
 };
@@ -74,7 +121,18 @@ const filterSensitiveUserData = (user) => {
   return rest;
 };
 
+const verifyPassword = async (inputPassword, savedPassword) => {
+  const isValid = await bcrypt.compare(inputPassword, savedPassword); // 변경
+  if (!isValid) {
+    const error = new Error("비밀번호가 일치 하지 않습니다.");
+    error.code = 401;
+    throw error;
+  }
+};
+
 export default {
+  createToken,
+  getUser,
   getUserByEmail,
   getUserById,
   create,
