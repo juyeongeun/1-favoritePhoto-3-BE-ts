ea0c732582cc23cc6adb1422b66f54bd11f583a6
S2OHEE <s2h110@naver.com>
2024-10-12 21:32:30 +0900

    refactor: 유효성 검사 미들웨어 개선 (하나의 export로 통합, totalCount가 0일때 및 next() 호출 추가

--- middlewares/shop/shopValidation.js
+++ middlewares/shop/shopValidation.js
@@ -1,25 +1,35 @@
-export const shopValidation = (req, res, next) => {
+/* 판매할 포토카드 등록 시, 유효성 검사 */
+const shopValidation = (req, res, next) => {
   const { price, totalCount } = req.body;
   if (!price || price < 0) {
     return res.status(400).json({ message: "가격을 입력해주세요." });
   }
 
-  if (!totalCount || totalCount < 0) {
+  // totalCount가 0보다 작거나 같은 경우에 대한 체크
+  if (!totalCount || totalCount <= 0) {
     return res.status(400).json({ message: "판매 수량을 입력해주세요." });
   }
+
+  next();
 };
 
-export const shopEditValidation = (req, res, next) => {
+/* 판매할 포토카드 수정 시, 유효성 검사 */
+const shopEditValidation = (req, res, next) => {
   const { price, totalCount, remainingCount } = req.body;
   if (!price || price < 0) {
     return res.status(400).json({ message: "가격을 입력해주세요." });
   }
 
-  if (!totalCount || totalCount < 0) {
+  // totalCount가 0보다 작거나 같은 경우에 대한 체크
+  if (!totalCount || totalCount <= 0) {
     return res.status(400).json({ message: "판매 수량을 입력해주세요." });
   } else if (totalCount < remainingCount) {
     return res
       .status(400)
       .json({ message: "남은 카드의 수량을 확인해주세요." });
   }
+
+  next();
 };
+
+export { shopValidation, shopEditValidation };
