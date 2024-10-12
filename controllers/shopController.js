d4c103cdb4773660d0bf72f04b136fa50835af27
S2OHEE <s2h110@naver.com>
2024-10-12 18:12:45 +0900

    refactor : 삳세조회 관련 코드 수정 및 주석 추가

--- controllers/shopController.js
+++ controllers/shopController.js
@@ -1,14 +1,13 @@
+//controllers\shopController.js
+
 import express from "express";
 import * as shopService from "../services/shopService.js";
 import asyncHandle from "../utils/error/asyncHandle.js";
-import {
-  shopValidation,
-  shopEditValidation,
-} from "../middlewares/shop/shopValidation.js";
+import { shopValidation } from "../middlewares/shop/shopValidation.js";
 
 const router = express.Router();
 
-// 카드 판매 등록하기
+// 판매할 포토카드 등록하기
 router.post(
   "/",
   shopValidation, // 요청 본문의 유효성을 검사하는 미들웨어
@@ -39,50 +38,23 @@ router.post(
   })
 );
 
-// 상점에 등록된 카드 목록 조회 라우트
-router.get(
-  "/cards/:shopId",
-  asyncHandle(async (req, res, next) => {
-    const {
-      page = 1,
-      pageSize = 9,
-      orderBy = "recent",
-      keyword = "",
-      grade = "",
-      genre = "",
-      isSoldOut = "",
-    } = req.query;
-
-    const filters = {
-      page: parseInt(page, 10),
-      pageSize: parseInt(pageSize, 10),
-      orderBy,
-      keyword,
-      grade,
-      genre,
-      isSoldOut:
-        isSoldOut === "true" ? true : isSoldOut === "false" ? false : undefined,
-    };
-
-    const { cards, totalCount } = await shopService.getShopCards(filters);
-    return res.status(200).json({ cards, totalCount });
-  })
-);
-
-// 포토 카드 상세 조회
+// 판매중인 포토 카드 상세 조회
 router.get(
   "/cards/:shopId/:cardId",
   asyncHandle(async (req, res, next) => {
-    const { cardId } = req.params;
-    const cardDetails = await shopService.getShopCardById(parseInt(cardId, 10));
+    const { shopId, cardId } = req.params;
+    const cardDetails = await shopService.getShopByShopId(
+      parseInt(shopId, 10), // shopId로 상점 확인
+      parseInt(cardId, 10) // cardId로 카드 확인
+    );
     return res.status(200).json(cardDetails);
   })
 );
 
 // 판매 중인 카드 수정하기
 router.patch(
-  "/:shopId",
-  shopEditValidation,
+  "/cards/:shopId/:cardId",
+  shopValidation, // 등록할때와 같은 shop 유효성검사 미들웨어 사용
   asyncHandle(async (req, res, next) => {
     const { shopId } = req.params;
     const {
@@ -100,37 +72,21 @@ router.patch(
       exchangeGrade,
       exchangeGenre,
       exchangeDescription,
+      userId: req.user.id, // 수정 요청 하는 사용자의 ID 추가
     });
 
     return res.status(200).json(updatedCard);
   })
 );
 
-// 판매 철회
+// 판매중인 카드 판매 취소(판매 내리기)
 router.delete(
-  "/:shopId",
+  "/cards/:shopId/:cardId",
   asyncHandle(async (req, res, next) => {
     const { shopId } = req.params;
-    await shopService.deleteShopCard(parseInt(shopId, 10));
+    const userId = req.user.id; // JWT로부터 사용자 ID 가져오기
+    await shopService.deleteShopCard(parseInt(shopId, 10), userId);
     return res.status(200).json({ message: "삭제되었습니다." });
-  })
-);
-
-// 카드 구매하기
-router.post(
-  "/:shopId/purchase",
-  asyncHandle(async (req, res, next) => {
-    const { shopId } = req.params;
-    const { count, userId } = req.body; // 판매자 id
-    const buyerId = req.user.id; // 구매자 ID
-
-    const result = await shopService.purchaseShopCard({
-      shopId: parseInt(shopId, 10),
-      count,
-      userId,
-      buyerId,
-    });
-    return res.status(200).json(result);
   })
 );
 
