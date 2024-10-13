1f6367f796cd0216b1be6f1c755f9afbd7d7d3b5
S2OHEE <s2h110@naver.com>
2024-10-13 20:44:15 +0900

    feat : 교환희망설명 글자수 제한하도록 유효성 검사 미들웨어에 추가

--- middlewares/shop/shopValidation.js
+++ middlewares/shop/shopValidation.js
@@ -15,18 +15,24 @@ const shopValidation = (req, res, next) => {
 
 /* 판매할 포토카드 수정 시, 유효성 검사 */
 const shopEditValidation = (req, res, next) => {
-  const { price, totalCount, remainingCount } = req.body;
+  const { price, totalCount, remainingCount, exchangeDescription } = req.body;
+
   if (!price || price < 0) {
     return res.status(400).json({ message: "가격을 입력해주세요." });
   }
 
-  // totalCount가 0보다 작거나 같은 경우에 대한 체크
   if (!totalCount || totalCount <= 0) {
     return res.status(400).json({ message: "판매 수량을 입력해주세요." });
   } else if (totalCount < remainingCount) {
     return res
       .status(400)
       .json({ message: "남은 카드의 수량을 확인해주세요." });
+  }
+
+  if (exchangeDescription && exchangeDescription.length > 30) {
+    return res
+      .status(400)
+      .json({ message: `교환 희망 설명은 30자 이하로 입력해주세요.` });
   }
 
   next();
