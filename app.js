73763bd6f2fc748f17b27edbd007a90a644c0366
twh <owootak@naver.com>
2024-10-17 10:22:52 +0900

    fix : port number

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(
   cors({
     credentials: true,
-    origin: "http://localhost:3001",
+    origin: "http://localhost:3000",
     exposedHeaders: ["set-cookie"],
   })
 );
