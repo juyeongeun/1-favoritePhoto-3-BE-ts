a8a0fde495e10029dda1f41246bb76fffd04f23e
juyeongeun <80696592+juyeongeun@users.noreply.github.com>
2024-10-18 13:30:14 +0900

    fix: 코드 정리

--- services/cardService.js
+++ services/cardService.js
@@ -41,11 +41,7 @@ const getByUserId = async (userId, data) => {
 
 // 카드 생성
 const createCard = async (data) => {
-  try {
-    return await cardRepository.createCard(data);
-  } catch (error) {
-    throw new Error("카드 생성 중 오류 발생");
-  }
+  return await cardRepository.createCard(data);
 };
 
 export default { createCard, getByUserId };
