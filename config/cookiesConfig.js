925a1ba08a3e981854300a146fe0eb6954fb6463
twh <owootak@naver.com>
2024-10-22 11:09:24 +0900

    쿠키 samesite

--- config/cookiesConfig.js
+++ config/cookiesConfig.js
@@ -1,14 +1,14 @@
 const accessTokenOption = {
   httpOnly: true,
   secure: true,
-  // sameSite: "None", // 개발환경에서만 적용
+  sameSite: "None", // 개발환경에서만 적용
   maxAge: 1000 * 60 * 60, //1시간
 };
 
 const refreshTokenOption = {
   httpOnly: true,
   secure: true,
-  // sameSite: "None", // 개발환경에서만 적용
+  sameSite: "None", // 개발환경에서만 적용
   maxAge: 1000 * 60 * 60 * 24, //1일,
 };
 
