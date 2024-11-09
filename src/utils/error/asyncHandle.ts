bbe827914714ae2236386ddb7fbc5e4ca1d46c03
mac woohyun <owootak@naver.com>
2024-11-09 17:07:52 +0900

    비동기 핸들러 타입정의 변경

--- src/utils/error/asyncHandle.ts
+++ src/utils/error/asyncHandle.ts
@@ -1,6 +1,6 @@
 import { Request, Response, RequestHandler, NextFunction } from "express";
 
-const asyncHandle = (handle: (req: Request, res: Response, next: NextFunction) => Promise<Response>) => {
+const asyncHandle = (handle: (req: Request, res: Response, next: NextFunction) => Promise<Response | void>) => {
   return (req: Request, res: Response, next: NextFunction) => {
     handle(req, res, next).catch(next);  // 비동기 함수에서 발생한 오류를 처리
   };
